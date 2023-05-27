508a801cc966afdab2f4a7eea11fd879f36b4033
Rafael Beraldo <rafael.pimpa@gmail.com>
2023-05-26 22:03:14 -0300

    use lsp-zero

--- lua/rafael/plugins/lsp-zero.lua
+++ lua/rafael/plugins/lsp-zero.lua
@@ -1,41 +1,46 @@
 return {
-  --   {
-  --     'VonHeikemen/lsp-zero.nvim',
-  --     branch = 'v2.x',
-  --     dependencies = {
-  --       -- LSP Support
-  --       { 'neovim/nvim-lspconfig' },
-  --       {
-  --         'williamboman/mason.nvim',
-  --         build = function()
-  --           pcall(vim.cmd, 'MasonUpdate')
-  --         end,
-  --       },
-  --       { 'williamboman/mason-lspconfig.nvim' },
-  --
-  --       -- Autocompletion
-  --       { 'hrsh7th/nvim-cmp' },
-  --       { 'hrsh7th/cmp-nvim-lsp' },
-  --       { 'L3MON4D3/LuaSnip' },
-  --     },
-  --     config = function()
-  --       local lsp = require('lsp-zero').preset({})
-  --
-  --       lsp.on_attach(function(client, bufnr)
-  --         lsp.default_keymaps({ buffer = bufnr })
-  --       end)
-  --
-  --       lsp.setup()
-  --
-  --       local cmp = require('cmp')
-  --
-  --       cmp.setup({
-  --         mapping = {
-  --           ['<CR>'] = cmp.mapping.confirm({ select = false }),
-  --         },
-  --       })
-  --
-  --       vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help)
-  --     end,
-  --   },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      {
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
+      { 'folke/neodev.nvim', opts = {} },
+    },
+    config = function()
+      local lsp = require('lsp-zero').preset({})
+
+      lsp.on_attach(function(client, bufnr)
+        lsp.default_keymaps({ buffer = bufnr })
+        vim.keymap.set({ 'n', 'i', 'v' }, '<C-h>', vim.lsp.buf.signature_help, { buffer = true })
+
+        lsp.buffer_autoformat()
+      end)
+
+      lsp.setup()
+
+      local cmp = require('cmp')
+
+      cmp.setup({
+        mapping = {
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<C-Space>'] = cmp.mapping.complete(),
+        },
+      })
+
+      vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help)
+    end,
+  },
 }
