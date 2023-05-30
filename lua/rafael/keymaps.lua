dcfb3d2319be722104949c2ef40787b89c664506
Rafael Beraldo <rafael.pimpa@gmail.com>
2023-05-30 17:04:03 -0300

    add more hotkeys

--- lua/rafael/keymaps.lua
+++ lua/rafael/keymaps.lua
@@ -1,12 +1,12 @@
 -- [[ Basic Keymaps ]]
 
 -- Modes cheatsheet:
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
+--   normal       = "n",
+--   insert       = "i",
+--   visual_      = "v",
+--   visual block = "x",
+--   terminal     = "t",
+--   command      = "c",
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
@@ -17,13 +17,16 @@ vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<C-s>', function()
 end)
 
 -- Delete only (no cut)
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"_d')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"_d', { desc = 'Delete without overwriting clipboard' })
 
 -- Paste without overwriting clipboard
 vim.keymap.set({ 'v', 'x' }, 'p', '"_dP')
 
+-- Copy to system clipboard
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
+
 -- New line below without leaving normal mode
-vim.keymap.set('n', '<leader>o', 'o<Esc>')
+vim.keymap.set('n', 'O', 'o<Esc>')
 
 -- Move lines
 vim.keymap.set('v', '<A-Up>', ":m '<-2<CR>gv=gv")
@@ -55,18 +58,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>t', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic message' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open diagnostic message' })
 
 -- Buffers keymaps
 vim.keymap.set('n', '<leader>q', vim.cmd.bd, { desc = 'Delete buffer' })
-vim.keymap.set('n', '<leader>bn', vim.cmd.bnext, { desc = 'Next buffer' })
-vim.keymap.set('n', '<leader>bN', vim.cmd.bprev, { desc = 'Previous buffer' })
+vim.keymap.set('n', ']b', vim.cmd.bnext, { desc = 'Next buffer' })
+vim.keymap.set('n', '[b', vim.cmd.bprev, { desc = 'Previous buffer' })
 
--- Find selected
--- vim.keymap.set('v', '<leader>s', 'y/<C-r><C-w>')
+-- Replace text on cursor
+vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>//g<Left><Left>]], { desc = 'Replace text on cursor' })
+
+-- Clear highlight
+vim.keymap.set('n', '<leader>c', ':let @/ = ""<CR>', { desc = 'Clear highlight' })
 
 -- Resize splits
 vim.keymap.set('n', '<A-kMinus>', ':resize -2<CR>')
