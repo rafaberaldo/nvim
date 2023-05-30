518feaa66d00b1c208c708e422c8c8c4d5a8cd8c
Rafael Beraldo <rafael.pimpa@gmail.com>
2023-05-30 13:57:50 -0300

    use builtin var

--- lua/rafael/plugins/telescope.lua
+++ lua/rafael/plugins/telescope.lua
@@ -10,9 +10,9 @@ return {
       vim.keymap.set('n', '<C-p>', function()
         vim.fn.system('git rev-parse --is-inside-work-tree')
         if vim.v.shell_error == 0 then
-          require('telescope.builtin').git_files()
+          builtin.git_files()
         else
-          require('telescope.builtin').find_files()
+          builtin.find_files()
         end
       end)
       vim.keymap.set('n', '<C-f>', builtin.live_grep)
