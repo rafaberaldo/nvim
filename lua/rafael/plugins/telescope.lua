027273687555ee1724ec3ebcb6d1450c5d00ee7b
Rafael Beraldo <rafael.pimpa@gmail.com>
2023-05-30 21:58:58 -0300

    fix telescope default order to asc

--- lua/rafael/plugins/telescope.lua
+++ lua/rafael/plugins/telescope.lua
@@ -4,8 +4,11 @@ return {
   branch = '0.1.x',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local builtin = require('telescope.builtin')
+    require('telescope').setup({
+      defaults = { sorting_strategy = 'ascending' },
+    })
 
+    local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files)
     vim.keymap.set('n', '<C-f>', builtin.live_grep)
     vim.keymap.set('n', '<leader><BS>', builtin.buffers)
